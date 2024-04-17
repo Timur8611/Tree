89bcf989bdfa7f7bf1eb8f1122ed6dcb4f0baac3
Timur <sirakanyantimur1@gmail.com>
2024-04-17 22:01:58 +0400

    Добавьте файлы проекта.

---
+++ Tree/ME.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    internal class Person
+    {
+        public Person() { }
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name, int age)
+
+        {
+            Name = name;
+            Age = age;
+        }
+        private string Name { get; }
+        private int Age { get; }
+        public Person MAM { get; private set; }
+        public Person PAP { get; private set; }
+        public Person WifeOrHusbent { get; private set; } = null;
+        public Person[] Children { get; private set; }
+        public void FamilyInfo(Person mother, Person father, Person wifeorhusbant, params Person[]children)
+        {
+            MAM = mother;
+            PAP = father;
+            WifeOrHusbent = wifeorhusbant;
+            Children = children;
+
+        }
+        public static void PrintME(Person person)
+        {
+            Console.Write($"Name : {person.Name}");    
+        }
+       
+       
+        public bool IsChildFree(Person person)
+        {
+            bool isChildfree = false;
+            if (person.Children.Length <= 0)
+            {
+                isChildfree = true;
+                return isChildfree;
+
+            }
+            else {return isChildfree;}
+
+
+        }
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person("Timur", 29);
+            Person mother = new Person("Asiya", 50);
+            Person father = new Person("Meruj", 55);
+            Person wife = new Person("Tata", 37);
+            Person child = new Person("Alise", 12);
+            Person grandmamM = new Person("Alevtina", 71);
+            Person grandpapM = new Person("Jemil", 72);
+            Person grandpapP = new Person("Aramais", 92);
+            Person grandmamP = new Person("Gulnara", 92);
+            Person wifeMom = new Person("Karina", 70);
+            Person wifeFather = new Person("Garik", 71);
+
+            me.FamilyInfo(mother, father, wife, child);
+            mother.FamilyInfo(grandmamM, grandpapM, father, me);
+            father.FamilyInfo(grandmamP, grandpapP, mother, me);
+            child.FamilyInfo(wife, me, null);
+            wife.FamilyInfo(wifeMom, wifeFather, child);
+
+            Person.PrintME(grandmamP);
+            Person.PrintME(grandpapP);
+            Console.Write("\t");
+            Person.PrintME(grandmamM);
+            Person.PrintME(grandpapM);
+            Console.WriteLine();
+            Person.PrintME(father);
+            Person.PrintME(mother);
+            Console.WriteLine();
+            Person.PrintME(me);
+            Person.PrintME(wife);
+            Console.WriteLine();
+            Person.PrintME(child);  
+        }
+    }
+}
